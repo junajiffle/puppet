3fa11b50a6a5c7825267a8b444fc9cfea5cc0bd0
Juna Anna Thomas <junaannathomas@gmail.com>
2022-07-25 14:38:47 +0530

    Create ntp.pp

---
+++ ntp.pp
@@ -0,0 +1,12 @@
+package { 'ntp':
+  ensure => "present"
+}
+
+file { "/etc/ntp.conf":
+  ensure  => "present",
+  content => "server 0.centos.pool.ntp.org iburst\n"
+}
+
+service { "ntpd":
+  ensure => "running"
+}
